5ba173d2eff3766b001bbdda6abcacebc642808d
aaron <aaronlyn88@gmail.com>
2024-05-10 14:58:26 +0800

    init: new repo

--- app/root.tsx
+++ app/root.tsx
@@ -5,6 +5,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import stylesheet from "~/tailwind.css?url";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
